a4c0b50561df927fe473055e5b269e07a9da81a8
Gubbalanagasai <119146937+Gubbalanagasai@users.noreply.github.com>
2022-12-01 20:55:45 +0530

    Add files via upload

---
+++ python/class 1,2,3,4.py
@@ -0,0 +1,44 @@
+# BASICS
+# is used as comments
+# Every line executed in python is either 1.statement or 2.expression
+
+print("hello")
+print("hello world")
+print("lets start")
+
+print(150+250)
+print(200+250)
+
+# integers
+# float
+# string 
+# complex
+
+a= 366
+print(a)
+
+a=535
+print(id(a))
+
+print(1,2,3,"hey",1+5j,sep="-")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/class 5.py
@@ -0,0 +1,30 @@
+# OPERATORS AND EXPRESSIONS
+
+# + = ADDITION
+# - = SUBSTRACTION
+# / = Division
+# * = MULTIPLICATION
+# // = FLOOR DIVISION
+# ** = POWER OF
+# % = MOD
+
+print(20+5)
+
+print(20-5)
+
+print(20/2)
+
+print(20//2)
+
+print(20%3)
+
+print(5**5)
+
+#LOGICAL OPERATORS
+
+# true and False
+# true and 1
+# 1 and 0
+# 1 and 5
+
+# type(true)
